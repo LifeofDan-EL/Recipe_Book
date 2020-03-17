f248eab289bc75323985dfff2f04f03f810620da
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-03-17 15:23:53 +0100

    Ported UI design to Material Design, implementing CardView on Home Activity.

---
+++ app/src/main/java/com/danielanomfueme/recipebook/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.danielanomfueme.recipebook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.loginbutton)
+        button.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
